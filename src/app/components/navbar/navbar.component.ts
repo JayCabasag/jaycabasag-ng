5ad13824559bb8a3e4f9ed42f730c77e73a2bb9d
Jay Cabasag <jaycabasag1999@gmail.com>
2023-07-04 01:11:06 +0800

    Added new changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isScrolling: boolean = false;
+  isScrollingDown: boolean = false;
+  scrollTimeout: any;
+  expandNavbar: boolean = false
 
+  toggleNavbar(): void {
+    this.expandNavbar = !this.expandNavbar
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.isScrolling = true;
+    this.expandNavbar = false
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      this.isScrolling = false;
+    }, 200);
+  }
 }
